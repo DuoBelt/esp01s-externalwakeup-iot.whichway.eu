986f9480c1ad16827548427ddcf474632e8a6598
DuoBelt <mariuszwziatek@gmail.com>
2022-04-30 01:27:23 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
-#include <Arduino.h>
+/*
+ * ESP01s 
+ * Deep sleep mode example
 
+ */
+ 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
+
+  // Wait for serial to initialize.
+  while(!Serial) { }
+  
+  // Deep sleep mode for 30 seconds, the ESP01s wakes up by itself when GPIO XX  is connected to the RESET pin
+
+  //ESP.deepSleep(30e6); 
+  
+
+  Serial.println(" Hello Morning");
+  ESP.deepSleep(0); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
